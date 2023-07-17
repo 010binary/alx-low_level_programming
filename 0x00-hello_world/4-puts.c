ccf1b0005e4f3829185fe26b8910a04eaec3eb39
010binary <augustine32fg@gmail.com>
2023-07-17 07:31:59 -0700

    trial 7

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio.h> 
-/** 
-  * main - this is void 
-  * Return: instruction says return zero 
-*/ 
-int main(void) 
-{ 
-         puts("\"Programming is like building a multilingual puzzle"); 
-         return (0); 
+#include <stdio.h>
+/**
+  * main - this is void
+  * Return: instruction says return zero
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
