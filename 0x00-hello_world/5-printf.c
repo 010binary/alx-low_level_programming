7d3a51bb9326b31c59d2eb1a9dc08630f85088f7
010binary <augustine32fg@gmail.com>
2023-07-17 07:39:46 -0700

    trial 9

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include <stdio.h> 
-/** 
-  * main - this is the first point in the program 
-  * Return: the zero in parenthesis 
-*/ 
-int main(void) 
-{ 
-         printf("with proper grammar, but the outcome is a piece of art,\n"); 
-         return (0); 
+#include <stdio.h>
+/**
+  * main - this is the first point in the program
+  * Return: the zero in parenthesis
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
