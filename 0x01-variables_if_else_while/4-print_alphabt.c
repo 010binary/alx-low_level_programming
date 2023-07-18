b3533e0925e254f56735da442a0daa5e675f09e0
010binary <augustine32fg@gmail.com>
2023-07-17 22:20:49 -0700

    trial 8

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,18 +1,17 @@
-#include <stdio.h> 
-  
- /** 
-  * main - main function 
-  * Return: zero always 
- */ 
-  
- int main(void) 
- { 
-         char i; 
-  
-         for (i = 'a'; i <= 'z'; i++) 
-         if (i != 'e' && i != 'q') 
-                 putchar(i); 
-  
-         putchar('\n'); 
-         return (0); 
- }
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: zero always
+*/
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		if (i != 'e' && i != 'q')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: zero always
+*/
+
+int main(void)
+{
+         int n;
+
+	 for (n = '0'; n <= '9'; n++)
+		 putchar(n);
+	 putchar('\n');
+	 return (0);
+}
