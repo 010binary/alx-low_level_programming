57c7b801b0943ff38545c66357c7366890a62c23
010binary <augustine32fg@gmail.com>
2023-09-09 09:49:50 -0700

    trial peace3

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,11 +10,12 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
-	       	printf("%d is positive", n);
+		printf("%d is positive", n);
 	else if (n == 0)
 		printf("%d is zero", n);
 	else if (n < 0)
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,8 +8,10 @@
 int main(void)
 {
 	char i;
+
 	for (i = 'a'; i <= 'z'; i++)
 		putchar(i);
+
 	putchar('\n');
 	return (0);
 }
