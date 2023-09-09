a69944ebd8b74465d1879f23660f40c394689c95
010binary <augustine32fg@gmail.com>
2023-09-09 10:41:05 -0700

    trial peace3

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *main function
+ *prints numbers less than 10
+ *main function always returns 0
+ */
 
+
+int main(void)
+{
+	int x;
+	for(x = 0; x < 10; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return(0);
+}
