b8dea5c09f313f8bb5265be76b1dce6352fda7ac
010binary <augustine32fg@gmail.com>
2023-09-12 17:07:37 -0700

    0 putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main: Entry point of the program
+ *
+ * Return: zero for this program
+*/
+
+int main(void)
+{
+	char text[] = "_putchar\n";
+	int i = 0;
+
+	while (text[i] != '\0')
+	{
+		_putchar(text[i]);
+		i++;
+	}
+
+	return (0);
+}
