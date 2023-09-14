7ce3cdcf97759b8141aa82b3da7e160bd2ccf8cd
010binary <augustine32fg@gmail.com>
2023-09-14 16:34:56 -0700

    control

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit.
+ * @c: parameter
+ * Return: 1 if c is digit and 0 otherwise.
+ */
+
+int _isdigit(int c)
+
+{
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ * @a: first integer.
+ * @b: second integer.
+ * Return: result.
+ */
+
+int mul(int a, int b)
+
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
