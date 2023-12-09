a93749e6e16edde6a97de0cfbf0eb684c5c2fcb6
010binary <augustine32fg@gmail.com>
2023-12-09 08:46:30 -0800

    task 1

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - Prints all elements of a dlistint_t list
+ * @h: Pointer to the head of the list
+ * Return: Number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+    size_t nodes = 0;
+
+    while (h != NULL)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+        nodes++;
+    }
+
+    return nodes;
+}
