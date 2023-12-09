78a89d1025a1ac39cb3e65406c8812b6a0e3e097
010binary <augustine32fg@gmail.com>
2023-12-09 09:42:52 -0800

    task  8 and setting it to betty standard

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Returns the nth node of a dlistint_t linked list
+ * @head: Pointer to the head of the list
+ * @index: Index of the node, starting from 0
+ * Return: Pointer to the nth node, or NULL if the node does not exist
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count;
+
+	if (head == NULL)
+		return (NULL);
+
+	for (count = 0; head != NULL; count++)
+	{
+		if (count == index)
+			return (head);
+
+		head = head->next;
+	}
+
+	return (NULL);
+}
