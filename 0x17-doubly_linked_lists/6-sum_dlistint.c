85f41f7b6546988cadef0903c733604151d8e3e7
010binary <augustine32fg@gmail.com>
2023-12-09 09:51:24 -0800

    task  9 and setting it to betty standard

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * sum_dlistint - Returns the sum of all the data (n) of a dlistint_t list
+ * @head: Pointer to the head of the list
+ * Return: Sum of all the data, or 0 if the list is empty
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+
+	if (head == NULL)
+		return (0);
+
+	sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
